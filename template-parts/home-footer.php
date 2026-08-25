a1860c88a03707adaf761d035ac9c47a1c4c5df6
Viralvic <104889740+Viralvic@users.noreply.github.com>
2026-08-25 06:30:22 +0100

    Homepage chrome v0.6: add template-parts/home-footer.php

---
+++ template-parts/home-footer.php
@@ -0,0 +1,9 @@
+<footer class="home-footer">
+  <div class="shell home-footer__grid">
+    <div><p class="home-footer__title">Company</p><nav aria-label="Footer navigation"><a href="<?php echo esc_url(home_url('/')); ?>">Home</a><a href="<?php echo esc_url(home_url('/about/')); ?>">About</a><a href="<?php echo esc_url(home_url('/advertise/')); ?>">Advertise</a><a href="<?php echo esc_url(home_url('/gt-content/')); ?>">GT Content</a><a href="<?php echo esc_url(gtvafrik_posts_page_url()); ?>">News</a><a href="#contact">Contact</a></nav></div>
+    <div><p class="home-footer__title">We Are Available 24/7. Call Now.</p><a href="tel:+2348188059300">+234 818 805 9300</a><a href="mailto:info@gtvafrik.com">info@gtvafrik.com</a><p class="home-footer__title home-footer__social-title">Find us here</p><div class="home-footer__social"><a href="#" aria-label="Facebook">f</a><a href="#" aria-label="YouTube">▶</a><a href="#" aria-label="X">𝕏</a><a href="#" aria-label="Instagram">◎</a><a href="#" aria-label="TikTok">♪</a></div></div>
+    <div><p class="home-footer__title">Contact Information</p><address>Suite 38 (3rd Floor) Birgi Plaza,<br>697 Idris Gidado Street,<br>Wuye District,<br>Abuja, Nigeria</address></div>
+  </div>
+  <div class="home-footer__legal"><div class="shell"><nav><a href="<?php echo esc_url(home_url('/disclaimer/')); ?>">Disclaimer</a><a href="<?php echo esc_url(home_url('/cookies-policy/')); ?>">Cookies Policy</a><a href="<?php echo esc_url(home_url('/privacy-policy/')); ?>">Privacy Policy</a><a href="<?php echo esc_url(home_url('/terms-of-use/')); ?>">Terms of Use</a></nav><p>Copyright © 2026 GTV Afrik | All Rights Reserved</p></div></div>
+</footer>
+
